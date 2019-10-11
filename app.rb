2c273e10f485a28351a845764c87ff382c2e12e4
Mark Hernandez <m.hern1415@gmail.com>
2019-10-11 22:42:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,19 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
-    
     erb :index
-    
   end
   
   get '/new' do
-    
   erb :create_puppy
-    
   end
   
   post '/puppy' do
-    
   @new_puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
 
     erb :display_puppy
